9ff5478b0cedd6dd478b755f62aafac919df37bf
neyzar <lucas.narguet451@gmail.com>
2025-05-16 21:25:56 +0200

    Initial commit with cleaned history

---
+++ src/app/components/error/error.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error',
+  imports: [CommonModule],
+  templateUrl: './error.component.html',
+})
+export class ErrorComponent {
+
+}
